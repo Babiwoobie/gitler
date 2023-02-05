e1a6cf7c906292ae861374d50a8c35185af92dcc
Babiwoobie <121252301+Babiwoobie@users.noreply.github.com>
2023-02-05 19:46:21 +0200

    Qw

---
+++ oop osn/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        car qwe = new car();
+    }
+}
---
+++ oop osn/src/car.java
@@ -0,0 +1,7 @@
+public class car {
+    public class car extends Trans {
+        public Car(int _speed, float _weight, String _color, byte[] _coordinate) {
+            setval(_speed, _weight, _color, _coordinate);
+        }
+    }
+}
